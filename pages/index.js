d4cd52b8bb3e7af0c151af6a70f1d9568817577a
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-06-02 03:16:27 +0530

    feat : rendered a list of meetups with dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
 // ourdomain/
+import  MeetupList  from "../components/meetups/MeetupList";
+import { DUMMY_MEETUPS } from "../constant/dummy_meetups";
+
+function HomePage() {
+    return <div>
+        <MeetupList meetups={DUMMY_MEETUPS}/>
+    </div>
+}
+
+export default HomePage;
