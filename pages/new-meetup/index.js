2baf199649a0eb2f945df85a670384a9ffc04bb6
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-06-02 19:20:16 +0530

    feat : added a form for adding meetups

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,20 @@
 // ourdomain/new-meetup
+
+import React from 'react'
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+
+function NewMeetupPage() {
+    function addMeetupHandler(data) {
+        console.log(data)
+
+    }
+
+
+    return (
+        <div>
+            <NewMeetupForm onAddMeetup={addMeetupHandler} />
+        </div>
+    )
+}
+
+export default NewMeetupPage;
