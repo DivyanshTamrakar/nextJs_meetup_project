c92433b88be986578b339ed724cfc286f4208f1b
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-06-02 19:56:58 +0530

    refactor : navigate programmitically

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,17 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
+import {useRouter} from 'next/router'
 
-function MeetupItem({ image, title, address }) {
+function MeetupItem({ id, image, title, address }) {
+
+const router = useRouter();
+
+const showHandler = () => {
+
+  router.push('/'+id);
+}
+
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +23,7 @@ function MeetupItem({ image, title, address }) {
           <address>{address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showHandler}>Show Details</button>
         </div>
       </Card>
     </li>
