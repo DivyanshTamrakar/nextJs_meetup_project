00fa6258a009750d95108a132cbd5e78bfaf07ee
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-06-02 20:10:48 +0530

    feat : added show details component

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,19 @@
 // ourdomain/meetupId
+import React from 'react'
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+function MeetupDetails() {
+    return (
+        <div>
+            <MeetupDetail
+                title='A First Meetup'
+                address='sector 1234, Some area of banglore'
+                image='https://images.unsplash.com/photo-1477925181867-37d89ee64b58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=864&q=80'
+                description="Dummy-Content"
+            />
+
+        </div>
+    )
+}
+
+export default MeetupDetails
