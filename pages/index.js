38a2b40f52878c2a48f786f76e560109184a090d
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-06-03 22:28:01 +0530

    refactor : used getStaticProps
    One of the main feature of nextJs.

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 // ourdomain/
 import MeetupList from "../components/meetups/MeetupList";
 import { DUMMY_MEETUPS } from "../constant/dummy_meetups";
-import {useEffect,useState} from 'react'
-function HomePage() {
-    const [loadedMeetups, setloadedMeetups] = useState([])
 
 
-    useEffect(() => {
-        //doing fetching form the backend 
-        setloadedMeetups(DUMMY_MEETUPS)
+function HomePage(props) {
+    return <MeetupList meetups={props.meetups} />
+}
 
-    }, [])
+export async function getStaticProps() {
+    // fetch the data from the server
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        }
+    }
 
-    return <div>
-        <MeetupList meetups={loadedMeetups} />
-    </div>
 }
 
 export default HomePage;
