81e086a115f0c664520381cc47d6531f197f9383
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2022-06-03 20:25:48 +0530

    refactor : applied state  on home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 // ourdomain/
 import MeetupList from "../components/meetups/MeetupList";
 import { DUMMY_MEETUPS } from "../constant/dummy_meetups";
-
+import {useEffect,useState} from 'react'
 function HomePage() {
+    const [loadedMeetups, setloadedMeetups] = useState([])
+
+
+    useEffect(() => {
+        //doing fetching form the backend 
+        setloadedMeetups(DUMMY_MEETUPS)
+
+    }, [])
+
     return <div>
-        <MeetupList meetups={DUMMY_MEETUPS} />
+        <MeetupList meetups={loadedMeetups} />
     </div>
 }
 
